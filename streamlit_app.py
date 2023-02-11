f7d6c773ce6a07bdb2d1867f17708c05d40446b1
Yogesh RameshKumar <34777203+yogeshrameshkumar@users.noreply.github.com>
2023-02-11 22:36:46 +0530

    Update streamlit_app.py
    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 
 streamlit.title('My parents new Healthy Dinner')
 
-streamlit.header('My parents new Healthy Dinner')
-
-streamlit.text('My parents new Healthy Dinner')
-streamlit.text('My parents new Healthy Dinner')
-streamlit.text('My parents new Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
